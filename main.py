3b24706308f837c39c825ec78b9bb7bee684b27b
Sohail Abbas <88001204+SohailBagoro@users.noreply.github.com>
2024-06-07 00:27:43 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import datetime
+
+def convertIDtoUnix(id):
+    """Converts an ID to UNIX timestamp."""
+    # Note: id has to be str
+    bin_str = bin(int(id))[2:]
+    m = 64 - len(bin_str)
+    unix_bin = bin_str[:42-m]
+    unix = int(unix_bin, 2) + 1420070400000
+    return unix
+
+def convert(id):
+    """Converts an ID to date."""
+    unix = convertIDtoUnix(str(id))
+    timestamp = datetime.datetime.utcfromtimestamp(unix / 1000)
+    print("Date:", timestamp.strftime('%Y-%m-%d, %H:%M:%S'))
+    print("timestamp:", unix)
+
+# Example usage
+convert("1169415091662364833")
