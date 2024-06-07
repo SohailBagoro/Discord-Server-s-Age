5cd8abb11caaeb98f9561eb602ee92c7040e1287
Sohail Abbas <88001204+SohailBagoro@users.noreply.github.com>
2024-06-07 11:23:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def convert(id):
     """Converts an ID to date."""
     unix = convertIDtoUnix(str(id))
     timestamp = datetime.datetime.utcfromtimestamp(unix / 1000)
-    print("Date:", timestamp.strftime('%Y-%m-%d, %H:%M:%S'))
+    print("Date:", timestamp.strftime('%Y-%m-%d, %H:%M:%S')  +  " UTC")
     print("timestamp:", unix)
 
 # Example usage
